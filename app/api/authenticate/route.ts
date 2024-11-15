e0a8827cd68021c786e76221bd1313c00bb0e6cb
Your Name <you@example.com>
2024-11-15 16:35:54 +0300

    ppp

--- app/api/authenticate/route.ts
+++ app/api/authenticate/route.ts
@@ -1,17 +1,29 @@
-// import axios from 'axios';
+import axios from 'axios';
 import { NextRequest } from 'next/server';
 
 export async function GET(req: NextRequest) {
 
   const initData = req.nextUrl.searchParams.get('initData');
   
-  console.log(initData);
-  
   if (!initData) {
     return new Response(JSON.stringify({ error: 'initData is missing' }), { status: 400 });
   }
+
+  try {
+    const response = await axios.get('http://localhost:8085/server/auth/authenticate', {
+      params: {
+        initData: initData
+      }
+    });
+} catch (error) {    
+    console.error('Authentication failed:', error);       
+    return new Response(JSON.stringify({ error: 'Authentication failed' }), { status: 500 });
+}
+    
 
   // Добавьте здесь логику обработки, если initData существует
   // Например, если вы хотите вернуть успешный ответ:
   return new Response(JSON.stringify({ message: 'initData received successfully', data: initData }), { status: 200 });
 }
+
+
