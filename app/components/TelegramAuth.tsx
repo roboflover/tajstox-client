e4956ad9293f4a67bb094062fbcfc1116c83944b
Your Name <you@example.com>
2024-11-17 15:33:26 +0300

    privet

--- app/components/TelegramAuth.tsx
+++ app/components/TelegramAuth.tsx
@@ -6,62 +6,29 @@ import axios from "axios";
 import { retrieveLaunchParams } from '@telegram-apps/sdk';
 
 const TelegramAuth: React.FC = () => {
-  const [token, setToken] = useState<string | null>(null);
-  const [telegramInitData, setTelegramInitData] = useState<string>('null');
-  
 
-
-  const authenticateUser = async (initData: string) => {
-    const { initDataRaw  } = retrieveLaunchParams();
-    console.log('initDataRaw', initDataRaw)
-  
-
-    // try {
-    //   const response = await axios.get(`/api/authenticate`, {
-    //     params: {
-    //       initData: initData
-    //     }
-    //   });
+  const sendInitDataToServer = async () => {
+    const { initDataRaw } = retrieveLaunchParams(); // Get init data
     
-    //   if (response.status !== 200) {
-    //     throw new Error('Failed to authenticate');
-    //   }
-    
-    //   const data = 'тест'//response.data;
-    //   setToken(data);
-    // } catch (error) {
-    //   console.error('Authentication failed:', error);
-    // }
-  }
-  
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://telegram.org/js/telegram-web-app.js';
-    script.defer = true;
-    document.head.appendChild(script);
+    try {
+      // Send init data to the server
+      const response = await axios.post('server/api/authenticate', {}, {
+        headers: {
+          Authorization: `tma ${initDataRaw}`, // Add init data in headers
+        },
+      });
 
-    script.onload = () => {
-      if (window.Telegram) {
-        const tgInfo = window.Telegram.WebApp.initData;
-        setTelegramInitData(tgInfo);
-        if(tgInfo === ''){
-          setTelegramInitData(objectToQueryString(mockTelegramData))
-        }
-      }
-    };
-
-    return () => {
-      document.head.removeChild(script);
-    };
-  }, []);
-  
+      console.log('Server Response:', response.data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
 
   return (
-    <div className="">
-          <button onClick={() => authenticateUser(telegramInitData)}>Тестовая регистрация</button>
-          {token}
-      {/* Your component content */}
+    <div>
+      <h1>Telegram Auth Example</h1>
+      <button onClick={sendInitDataToServer}>Send Init Data</button>
     </div>
   );
 };
