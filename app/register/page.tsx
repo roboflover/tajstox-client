896d4f05b4a6d1ec9773c28cb25b01f63d06dc14
Your Name <you@example.com>
2024-12-07 09:12:00 +0300

    ppp

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useState, FormEvent } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 interface RegisterResponse {
   message: string;
