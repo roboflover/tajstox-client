0019c186f4db2234f8c994a42ebd7c5bb8a1c6d1
Your Name <you@example.com>
2024-12-18 10:26:43 +0300

    ppp

--- app/api/referralCount/route.ts
+++ app/api/referralCount/route.ts
@@ -5,29 +5,32 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
   const host = process.env.NEXT_PUBLIC_SERVER;
+  console.log('Incoming request to /api/referralCount');
 
   // Извлечение JWT из cookies
   const jwtToken = req.cookies.get('jwtToken');
 
   if (!jwtToken) {
-    // Возвращаем ошибку, если токен отсутствует
+    console.error('JWT token not found in request cookies');
     return NextResponse.json({ success: false, error: 'Unauthorized' }, { status: 401 });
   }
 
-  try {
+  console.log('JWT token found, proceeding to fetch data');
 
+  try {
     const response = await axios.get(`${host}/server/users/referralCount`, {
       headers: {
         Authorization: `Bearer ${jwtToken.value}`,
       },
     });
 
-    // Возврат успешного ответа клиенту
+    console.log('Data successfully retrieved from the server', response.data);
+
     return NextResponse.json({ success: true, data: response.data });
   } catch (error) {
-    console.error('Error retrieving score:', error);
+    console.error('Error retrieving score from server:', error);
 
-    // Возврат ошибки клиенту
+
     return NextResponse.json({ success: false, error: 'Failed to retrieve score.' }, { status: 500 });
   }
 }
