d9cb4676e550391862a02bbcb3c6228fe63fe84c
Your Name <you@example.com>
2024-12-19 20:29:30 +0300

    ppp

--- app/team/components/InviteFriend.tsx
+++ app/team/components/InviteFriend.tsx
@@ -37,10 +37,10 @@ const InviteFriend = forwardRef<InviteFriendRef, InviteFriendProps>((props, ref)
   }, [token]);
 
   const referralLink = useMemo(
-    () => (userId ? `https://t.me/Tajstoxbot?startapp=${userId}` : ''),
+    () => (userId ? `https://t.me/share/url?url=https://t.me/Tajstoxbot?startapp=${userId}` : ''),
     [userId]
   );
-
+  // https://t.me/share/url?url=https://t.me/Tajstoxbot?startapp=${userId}&text=${encodedText}
   const copyToClipboard = () => {
 
     console.log('Window.Telegram:', window.Telegram);
@@ -81,12 +81,7 @@ const InviteFriend = forwardRef<InviteFriendRef, InviteFriendProps>((props, ref)
 
   return (
     <div>
-      <button
-        onClick={copyToClipboard}
-        className="flex items-center px-6 py-3 font-semibold text-white rounded-lg bg-gradient-to-r from-blue-500 to-purple-500 hover:from-blue-600 hover:to-purple-600 focus:outline-none focus:ring-2 focus:ring-blue-300 mt-20 space-x-2"
-      >
-        <span>Invite a friend</span>
-      </button>
+
     </div>
   );
 });
