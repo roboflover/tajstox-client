d701a48002cfe05c1e4e96a7769d46bda7123bff
Your Name <you@example.com>
2024-12-20 08:55:31 +0300

    ppp

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -26,10 +26,16 @@ const Team: React.FC = () => {
     }, []); 
 
     const handleCopyClick = () => {
-      if (inviteFriendRef.current) {
-        inviteFriendRef.current.copyToClipboard();
-      }
-    };
+        if (inviteFriendRef.current) {
+          inviteFriendRef.current.copyToClipboard();
+        }
+      };
+      
+      const handleClick = () => {
+        if (inviteFriendRef.current) {
+          inviteFriendRef.current.navigateToLink(); // Добавленный метод перехода по ссылке
+        }
+      };
 
     return (
         <div className="space-y-6 mt-20 mr-5 ml-5">  {/* Дополнительно добавили отступ между строками */}
@@ -40,12 +46,12 @@ const Team: React.FC = () => {
                     <HandshakeIcon  fontSize="large" sx={{ color: blue[500]}} />
                 </div>
                 <div className="flex items-center ml-2 ">
-                <span className="ml-2  font-semibold text-2xl">{referralCount} Friends</span>
+                <span className="ml-2  font-semibold text-2xl">0 {referralCount} Friends</span>
                 </div>
             </div>
             {/* Существующая центральная кнопка */}
             <div className="flex justify-center items-center">
-                <button onClick={handleCopyClick} className="flex items-center px-6 py-3 font-semibold text-white rounded-lg bg-gradient-to-r from-blue-500 to-purple-500 hover:from-blue-600 hover:to-purple-600 focus:outline-none focus:ring-2 focus:ring-blue-300 mt-20 space-x-2">
+                <button onClick={handleClick} className="flex items-center px-6 py-3 font-semibold text-white rounded-lg bg-gradient-to-r from-blue-500 to-purple-500 hover:from-blue-600 hover:to-purple-600 focus:outline-none focus:ring-2 focus:ring-blue-300 mt-20 space-x-2">
                     {/* Текст */}
                     <span>Invite a friend</span>
                 </button>
